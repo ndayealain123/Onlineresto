b7cdb604a2e2aef64a84eb6fc69cecebbb562b16
Alain NDAYE <ndayealain123>
2026-03-10 13:38:38 +0530

    fix media serving

--- app/urls.py
+++ app/urls.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
 from django.urls import path
-from .views import*
+from .views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',Home,name="home"),
-    path('menu/',Menuview,name="menu"),
-    path('social',Canteen),
+    path('admin/', admin.site.urls),
+    path('', Home, name="home"),
+    path('menu/', Menuview, name="menu"),
+    path('social', Canteen),
     path('register/', register_profil, name='register'),
     path('create_order/<int:pk>', Create_order, name='new_order'),
     path('login/', connexion, name='connect'),
     path('deconnexion/', deconnexion, name='deconnect'),
     path('order/', orderview, name='order'),
-    path('Media',Restoo),
-    path('about/',aboutview,name="about"),
-    path('finish/<int:pk>',finishOrder,name="finished"),
-    path('receive/<int:pk>',receiveOrder,name="received"),
-
-
-
-
-
-
+    path('Media', Restoo),
+    path('about/', aboutview, name="about"),
+    path('finish/<int:pk>', finishOrder, name="finished"),
+    path('receive/<int:pk>', receiveOrder, name="received"),
 ]
+
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
