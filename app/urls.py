58b9ccee2a2faa5382830f89e6285adf76527119
Alain NDAYE <ndayealain123>
2026-03-10 14:31:38 +0530

    fix media serving

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('receive/<int:pk>', receiveOrder, name="received"),
 ]
 
-# Serve media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
