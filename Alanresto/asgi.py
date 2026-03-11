a2072906983e1278977241b7a059b073253f487d
Alain NDAYE <ndayealain123>
2026-03-11 22:52:36 +0530

    Rename project to Alanresto and update structure

--- Alanresto/asgi.py
+++ Alanresto/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestaurantApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alanresto.settings')
 
 application = get_asgi_application()
--- Alanresto/wsgi.py
+++ Alanresto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestaurantApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alanresto.settings')
 
 application = get_wsgi_application()
